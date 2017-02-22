7915299cac15d3ff84bea9a5941e524b76daeb24
Derek Tamsen <dtamsen@gmail.com>
2017-02-21 23:51:02 -0800

    resolves 2 use viper to read config file and env vars to configure service

--- main.go
+++ main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
-	"github.com/derektamsen/hancock/aws"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/derektamsen/hancock/aws"
+	"github.com/derektamsen/hancock/opts"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	presigned_url := awsurl.PreSign(r.URL.Path[1:])
-	http.Redirect(w, r, presigned_url, http.StatusFound)
+// These are default configs and can be overridden in ./config.yaml or ENV vars prefixed
+// with HANCOCK_<var_name>
+var defConfigFile = "config"   // config file prefix name. Can end in yaml, json, toml
+var defListenPort = "8080"     // port the service listens for requests at
+var defAWSRegion = "us-east-1" // aws region service bucket is located
+var defS3Bucket = "testbucket" // bucket assets are located
+var defPresignTime = 15        // time in minutes url is valid
+var defAWSSvc = "s3"           // service to generate presigned url for
+
+// httpHandler signs urls using awsurl.S3PreSign then redirects the user to the new location
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	awsSvc := opts.GetConfigString("aws_svc", defAWSSvc)
+	var presignedURL string
+	switch awsSvc {
+	case "s3":
+		presignedURL = awsurl.S3PreSign(r.URL.Path[1:],
+			opts.GetConfigString("aws_region", defAWSRegion),
+			opts.GetConfigString("s3_bucket", defS3Bucket),
+			opts.GetConfigInt("presign_time", defPresignTime),
+		)
+	default:
+		log.Fatal("AWS Service not supported: ", awsSvc)
+	}
+
+	http.Redirect(w, r, presignedURL, http.StatusFound)
 }
 
+// main starts a http server listening for requests on <listen_host>:<listen_port>
 func main() {
-	httpserver := &http.Server{
-		Addr:           ":8080",
+	log.Print("Starting Hancock signing server")
+	opts.SetConfigLoad("hancock", defConfigFile)
+
+	listenPort := opts.GetConfigString("listen_port", defListenPort)
+
+	httpServer := &http.Server{
+		Addr:           ":" + listenPort,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	http.HandleFunc("/", handler)
-	log.Fatal(httpserver.ListenAndServe())
+
+	log.Print("Listening on port: ", listenPort)
+	http.HandleFunc("/", httpHandler)
+	log.Fatal(httpServer.ListenAndServe())
 }
